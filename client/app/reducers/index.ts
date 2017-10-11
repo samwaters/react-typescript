fc078fd7e58e3c381dc027f4297c72ce952da9b5
Sam Waters <sam@samwaters.com>
2017-10-11 16:14:56 +0100

    Theme reducer

--- client/app/reducers/index.ts
+++ client/app/reducers/index.ts
@@ -2,16 +2,19 @@ import {combineReducers} from 'redux';
 import {authReducer, IAuthState} from './auth.reducer';
 import {homeReducer, IHomeState} from './home.reducer';
 import {routerReducer} from 'react-router-redux';
+import {themeReducer, IThemeState} from "reducers/theme.reducer";
 
 interface IAppState {
-	auth: IAuthState;
-	home: IHomeState;
-	router: any;
+    auth: IAuthState;
+    home: IHomeState;
+    router: any;
+    theme: IThemeState;
 }
 
 const appReducers = combineReducers({
-	auth: authReducer,
-	home: homeReducer,
-	router: routerReducer
+    auth: authReducer,
+    home: homeReducer,
+    router: routerReducer,
+    theme: themeReducer
 });
 export {appReducers, IAppState};
