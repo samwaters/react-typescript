4380a8e111bde9caf65e085ed33a2c80325e1765
Sam Waters <sam@samwaters.com>
2017-10-18 16:11:21 +0100

    Better typing for props and dispatch

--- client/app/app.tsx
+++ client/app/app.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import {connect} from 'react-redux';
 import {Route, Switch} from 'react-router-dom';
 // Redux
+import {IConnectedProps} from 'actions/action.interface';
 import {setTheme} from 'actions/theme.actions';
 import {IAppState} from 'reducers/index';
 import {ITheme} from 'reducers/theme.reducer';
@@ -19,70 +20,67 @@ import NotFoundComponent from 'components/notfound';
 // Themes
 import '../theme/global';
 
-interface AppProps {
-    currentTheme: string;
-    dispatch?: (action: any) => void;
-    themes: { [key: string]: ITheme },
-    setTheme: (theme: string) => void;
+interface AppProps extends IConnectedProps {
+	currentTheme: string;
+	themes: { [key: string]: ITheme },
+	setTheme: (theme: string) => void;
 }
 
 interface AppState {
-    currentTheme: string;
+	currentTheme: string;
 }
 
 class App extends React.Component<AppProps, AppState> {
-    constructor() {
-        super();
-        this.state = {
-            currentTheme: 'theme1'
-        }
-    }
+	constructor() {
+		super();
+		this.state = {
+			currentTheme: 'theme1'
+		}
+	}
 
-    handleThemeChange(theme: string) {
-        this.setState({currentTheme: theme});
-        this.props.setTheme(theme);
-    }
+	handleThemeChange(theme: string) {
+		this.setState({currentTheme: theme});
+		this.props.setTheme(theme);
+	}
 
-    render() {
-        return <MuiThemeProvider muiTheme={getMuiTheme({palette: this.props.themes[this.props.currentTheme]})}>
-            <div style={{display: 'flex', flexDirection: 'column', height: '100vh'}}>
-                <AppBar
-                    title="React Starter"
-                    iconElementRight={<SelectField
-                        autoWidth
-                        floatingLabelText='Theme'
-                        onChange={(e: TouchTapEvent, i: number, v: any) => this.handleThemeChange(v)}
-                        value={this.state.currentTheme}
-                    >
-                        <MenuItem value='theme1' primaryText="Theme 1"/>
-                        <MenuItem value='theme2' primaryText="Theme 2"/>
-                        <MenuItem value='theme3' primaryText="Theme 3"/>
-                    </SelectField>}
-                />
-                <div style={{flex: 1, overflowY: 'auto', padding: '1.8rem'}}>
-                    <Switch>
-                        <Route component={HomeComponent} exact path="/"/>
-                        <AuthenticatedRoute component={HomeComponent} path="/secret" redirect="/login"/>
-                        <Route component={NotFoundComponent}/>
-                    </Switch>
-                </div>
-            </div>
-        </MuiThemeProvider>
-    }
+	render() {
+		return <MuiThemeProvider muiTheme={getMuiTheme({palette: this.props.themes[this.props.currentTheme]})}>
+			<div style={{display: 'flex', flexDirection: 'column', height: '100vh'}}>
+				<AppBar
+					title="React Starter"
+					iconElementRight={
+						<SelectField
+							autoWidth
+							floatingLabelText='Theme'
+							onChange={(e: TouchTapEvent, i: number, v: any) => this.handleThemeChange(v)}
+							value={this.state.currentTheme}
+						>
+							<MenuItem value='theme1' primaryText="Theme 1"/>
+							<MenuItem value='theme2' primaryText="Theme 2"/>
+							<MenuItem value='theme3' primaryText="Theme 3"/>
+						</SelectField>
+					}
+				/>
+				<div style={{flex: 1, overflowY: 'auto', padding: '1.8rem'}}>
+					<Switch>
+						<Route component={HomeComponent} exact path="/"/>
+						<AuthenticatedRoute component={HomeComponent} path="/secret" redirect="/login"/>
+						<Route component={NotFoundComponent}/>
+					</Switch>
+				</div>
+			</div>
+		</MuiThemeProvider>
+	}
 }
 
 const AppComponent = connect(
-    (state: IAppState) => {
-        return {
-            currentTheme: state.theme.currentTheme,
-            themes: state.theme.themes
-        }
-    },
-    (dispatch: any) => {
-        return {
-            setTheme: (theme: string) => dispatch(setTheme(theme))
-        }
-    }
+		(state: IAppState) => ({
+			currentTheme: state.theme.currentTheme,
+			themes: state.theme.themes
+		}),
+		(dispatch: Function) => ({
+			setTheme: (theme: string) => dispatch(setTheme(theme))
+		})
 )(App);
 
 export default AppComponent;
