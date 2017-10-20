2f22ef706410406a9fad752672e9fdcac89deb7e
Sam Waters <sam@samwaters.com>
2017-10-20 15:31:51 +0100

    Update typings for colours

--- client/theme/colours.css.d.ts
+++ client/theme/colours.css.d.ts
@@ -1,7 +1,31 @@
 interface Styles {
 [name:string]:string;
-colour1: string;
-colour2: string;
+purple1: string;
+purple2: string;
+purple3: string;
+purple4: string;
+green1: string;
+green2: string;
+green3: string;
+green4: string;
+blue1: string;
+blue2: string;
+blue3: string;
+blue4: string;
+pink1: string;
+pink2: string;
+pink3: string;
+pink4: string;
+orange1: string;
+orange2: string;
+orange3: string;
+orange4: string;
+yellow1: string;
+yellow2: string;
+yellow3: string;
+yellow4: string;
+black: string;
+white: string;
 }
 declare var styles:Styles;
 export = styles;
